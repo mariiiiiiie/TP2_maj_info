4645cf7a27239659ea6ce8ed943728450f666b86
Myriam <myriam.dasque@ensea.fr>
2023-12-13 16:19:14 +0100

    Server adress recuperation

---
+++ serverAdress.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h> 
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <netdb.h>
+#include <arpa/inet.h> 
+
+int main(int argc, char *argv[]){
+	
+	char *host, *file;
+	
+	if (argc<3){
+		printf("not enough arguments \n");
+		exit(EXIT_FAILURE);
+	}
+	else{
+		host = argv[1];
+		file = argv[2];
+		printf("the host is : %s \nthe file is : %s \n",host,file);
+	}
+	
+    struct addrinfo hints;
+    struct addrinfo *result,*rp;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;    // IPv4
+    hints.ai_socktype = SOCK_DGRAM; // socket for an UDP
+    hints.ai_flags = 0;
+    hints.ai_protocol = IPPROTO_UDP; // config prtocol for an UDP
+
+    
+    int s = getaddrinfo(argv[1], argv[2], &hints, &result);
+    
+    if (s != 0) { //verification if getaddrinfo worked
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s)); //return of the error code
+        exit(EXIT_FAILURE);
+    }
+    /* adress recuperation
+    for(rp = result; rp!=NULL; rp->ai_next){
+		void *addr;
+		struct sockaddr_in *ipv4 = (struct sockaddr_in *)rp->ai_addr;
+        addr = &(ipv4->sin_addr);
+        printf("L'adresse est : %p\n",addr);
+	}*/
+}
